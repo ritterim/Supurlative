1b4647ca1e42ef7efad516b961d32a30453fc2da
Thomas Harold <tgharold@gmail.com>
2015-09-04 14:24:44 -0400

    creating tests for SupurlativeOptions

--- tests/Core.Tests/SupurlativeOptionsTests.cs
+++ tests/Core.Tests/SupurlativeOptionsTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Web.Http;
-using Tavis.UriTemplates;
 using Xunit;
 
 namespace RimDev.Supurlative.Tests
@@ -35,48 +34,47 @@ namespace RimDev.Supurlative.Tests
         }
 
         [Fact]
-        public void Can_passthrough_uppercase_keys()
+        public void Can_pass_through_mixed_case_keys_from_anonymous_complex_route_properties()
         {
-            string expected = _baseUrl + "foo/1?Bar.Abc=abc&Bar.Def=def";
+            string expected = _baseUrl + "foo/{id}{?Bar.Abc,Bar.Def}";
             const string routeName = "foo.show";
             const string routeTemplate = "foo/{id}";
-
-            string template = TestHelper.CreateATemplateGenerator(_baseUrl, routeName, routeTemplate,
+            string actual = TestHelper.CreateATemplateGenerator(_baseUrl, routeName, routeTemplate,
                 supurlativeOptions: new SupurlativeOptions
                 {
                     LowercaseKeys = false,
                 })
                 .Generate(routeName, new { Id = 1, Bar = new { Abc = "abc", Def = "def" } });
-            var uriTemplate = new UriTemplate(template);
-
-            uriTemplate.AddParameters(new { id = 1 });
-            uriTemplate.AddParameter("Bar.Abc", "abc");
-            uriTemplate.AddParameter("Bar.Def", "def");
-
-            string actual = uriTemplate.Resolve();
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void Can_change_keys_to_lowercase()
+        public void Can_pass_through_upper_case_keys_from_anonymous_complex_route_properties()
         {
-            string expected = _baseUrl + "foo/1?bar.abc=abc&bar.def=def";
+            string expected = _baseUrl + "foo/{id}{?BAR.Abc,BAR.Def}";
             const string routeName = "foo.show";
             const string routeTemplate = "foo/{id}";
+            string actual = TestHelper.CreateATemplateGenerator(_baseUrl, routeName, routeTemplate,
+                supurlativeOptions: new SupurlativeOptions
+                {
+                    LowercaseKeys = false,
+                })
+                .Generate(routeName, new { Id = 1, BAR = new { Abc = "abc", Def = "def" } });
+            Assert.Equal(expected, actual);
+        }
 
-            string template = TestHelper.CreateATemplateGenerator(_baseUrl, routeName, routeTemplate,
+        [Fact]
+        public void Can_change_keys_to_lowercase_from_anonymous_complex_route_properties()
+        {
+            string expected = _baseUrl + "foo/{id}{?bar.abc,bar.def}";
+            const string routeName = "foo.show";
+            const string routeTemplate = "foo/{id}";
+            string actual = TestHelper.CreateATemplateGenerator(_baseUrl, routeName, routeTemplate,
                 supurlativeOptions: new SupurlativeOptions
                 {
                     LowercaseKeys = true,
                 })
                 .Generate(routeName, new { Id = 1, Bar = new { Abc = "abc", Def = "def" } });
-            var uriTemplate = new UriTemplate(template);
-
-            uriTemplate.AddParameters(new { id = 1 });
-            uriTemplate.AddParameter("Bar.Abc", "abc");
-            uriTemplate.AddParameter("Bar.Def", "def");
-
-            string actual = uriTemplate.Resolve();
             Assert.Equal(expected, actual);
         }
 
