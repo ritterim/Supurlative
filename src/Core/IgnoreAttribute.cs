720819c5c46972d12b6dcf647852395011f281ab
unknown <tom.harold@RIMDEV0103L.hq.ritterim.com>
2015-09-08 10:49:44 -0400

    create [IgnoreAttribute] for properties in classes

---
+++ src/Core/IgnoreAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RimDev.Supurlative
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreAttribute : Attribute
+    {
+        public IgnoreAttribute()
+        {
+            Ignore = true;
+        }
+
+        public bool Ignore { get; private set; }  
+    }
+}
