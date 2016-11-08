45a5952ac2f5ab4413330ea7ef71cec9c5ca2c46
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2016-11-08 16:16:15 -0500

    remember to save project and file

--- src/Paging/IPagedList.cs
+++ src/Paging/IPagedList.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RimDev.Supurlative.Paging
+﻿namespace RimDev.Supurlative.Paging
 {
     /// <summary>
     /// Not necessary to implement,
@@ -13,21 +11,5 @@ namespace RimDev.Supurlative.Paging
         int PageNumber { get; }
         int PageSize { get; }
         int TotalItemCount { get; }
-    }
-
-    internal static class DynamicBinderExtensions
-    {
-        internal static bool Has<TType, TReturn>(this TType target, Func<TType, TReturn> test)
-        {
-            try
-            {
-                var value = test(target);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
     }
 }
